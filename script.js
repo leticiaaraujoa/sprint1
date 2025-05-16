95f8548ba8e84ab071c18a3943083fd9cb2e0d83
leticiaaraujoa <laalvim@sga.pucminas.br>
2025-05-16 14:59:04 +0000

    atualização

--- script.js
+++ script.js
@@ -28,3 +28,10 @@ btnEnviar.addEventListener("click", () => {
     inputComentario.value = "";
   }
 });
+
+const url = encodeURIComponent(window.location.href);
+const titulo = encodeURIComponent(document.querySelector("h1").textContent);
+
+document.getElementById("compartilhar-facebook").href = `https://www.facebook.com/sharer/sharer.php?u=${url}`;
+document.getElementById("compartilhar-twitter").href = `https://twitter.com/intent/tweet?text=${titulo}&url=${url}`;
+document.getElementById("compartilhar-whatsapp").href = `https://wa.me/?text=${titulo}%20${url}`;
