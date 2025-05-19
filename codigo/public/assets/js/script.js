966ed81071471fbdda82eac5702110d065e5ff00
leticiaaraujoa <laalvim@sga.pucminas.br>
2025-05-19 12:39:14 +0000

    AUAU

--- codigo/public/assets/js/script.js
+++ codigo/public/assets/js/script.js
@@ -35,3 +35,16 @@ const titulo = encodeURIComponent(document.querySelector("h1").textContent);
 document.getElementById("compartilhar-facebook").href = `https://www.facebook.com/sharer/sharer.php?u=${url}`;
 document.getElementById("compartilhar-twitter").href = `https://twitter.com/intent/tweet?text=${titulo}&url=${url}`;
 document.getElementById("compartilhar-whatsapp").href = `https://wa.me/?text=${titulo}%20${url}`;
+
+async function getPosts() {
+  try{
+const p = await fetch(`https://upgraded-space-eureka-5gr6g7wxpp9724g9p-3000.app.github.dev/posts`);
+
+  console.log(p)
+  }catch(err) {
+    console.error(err)
+  }
+  
+}
+
+window.addEventListener("load", async () => {await getPosts()})
--- codigo/public/index.js
+++ codigo/public/index.js
@@ -5,8 +5,9 @@ const middlewares = jsonServer.defaults({noCors: true})
 server.use(middlewares)
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next()
 })
 
