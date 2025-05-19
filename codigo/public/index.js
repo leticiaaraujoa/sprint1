b7963e68495d5f84fd55286a636c76aefa3cf73a
leticiaaraujoa <laalvim@sga.pucminas.br>
2025-05-19 11:51:27 +0000

    Cachaça com mel

--- codigo/public/index.js
+++ codigo/public/index.js
@@ -1,13 +1,17 @@
 const jsonServer = require('json-server')
-const express = require('express')
-const path = require('path')
-const server = express()
+const server = jsonServer.create()
 const router = jsonServer.router('./db/db.json')
-const middlewares = jsonServer.defaults()
-server.use(express.static(path.join(__dirname, 'public')))
+const middlewares = jsonServer.defaults({noCors: true})
 server.use(middlewares)
-server.use('/api', router)
-server.listen(3000, () => {const router = jsonServer.router('./db/db.json')
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next()
+})
 
+server.use(router)
+
+server.listen(3000, () => {
   console.log('Servidor rodando em http://localhost:3000')
 })
